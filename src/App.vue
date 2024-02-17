efafb8b366831f63c859094642bb55cdc6dcd36b
Quentin Buteau <buteauquentin10@gmail.com>
2024-02-17 18:54:26 +0100

    test const dans app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+
+const test = "Je veux voir si ça marche"
 </script>
 
 <template>
   <header>
+    <h1>{{ test }}</h1>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
