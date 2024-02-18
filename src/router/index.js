46a1067e2e58ae673a62d8f2f6b3374e8da0e7fa
Jathushan Selvarajah <126668649+Jathushanselvarajah@users.noreply.github.com>
2024-02-18 22:54:07 +0100

    Débogage Home

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,19 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: () => import('/src/App.vue')
+      path: "/",
+      name: "home",
+      component: () => import("/src/views/Home.vue"),
     },
     {
-      path: '/presentation',
-      name: 'presentation',
-      component: () => import('/src/views/Presentation.vue')
-    }
-  ]
-})
+      path: "/presentation",
+      name: "presentation",
+      component: () => import("/src/views/Presentation.vue"),
+    },
+  ],
+});
 
-export default router
+export default router;
---
+++ src/views/Home.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import { ref, computed } from "vue";
+import router from "../router";
+import Presentation from "./Presentation.vue";
+</script>
+
+<template>
+  <section>
+    <h1>PAGE APP.VUE</h1>
+    <div class="presentation">
+      <div class="presentation__img">
+        <img src="@/assets/img/logo.png" alt="greenscore" height="350px" />
+      </div>
+      <div class="presentation__texte">
+        <div>
+          <h2>Qu'est-ce que GreenScore ?</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit
+            expedita quia labore! Pariatur provident ullam dolorum earum labore
+            praesentium, sapiente dolor exercitationem, vitae fuga recusandae.
+          </p>
+          <p>
+            Eveniet consequuntur error, sequi blanditiis explicabo, quam unde
+            aspernatur facilis maxime nesciunt nobis fuga eligendi aliquid quas
+            ipsam numquam excepturi maiores quibusdam ex, quo quia.
+          </p>
+          <p>
+            Minus, voluptas fugiat esse aperiam tempore quibusdam hic, quaerat
+            totam omnis aut laborum cupiditate pariatur a temporibus alias.
+            Explicabo maiores, sit incidunt fuga quibusdam dolorem.
+          </p>
+          <p>
+            Dolor incidunt nulla sint aliquam fugit numquam, vero officiis
+            voluptatem similique eos laborum ipsa perferendis! Sapiente
+            voluptates eos facere laboriosam. Ex sed aut neque eveniet!
+          </p>
+          <p>
+            Quos est quis illo ullam architecto doloremque, dolor laborum optio
+            soluta blanditiis adipisci quia nisi? Facere, fugit! Incidunt,
+            corporis dicta. Ullam qui cum deleniti velit.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped></style>
