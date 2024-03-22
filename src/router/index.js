ebe7dce5761e044bb987d38dcf5f304332045f86
Jathushan Selvarajah <126668649+Jathushanselvarajah@users.noreply.github.com>
2024-03-22 11:45:50 +0100

    Commit et delete des fichier test

--- src/router/index.js
+++ src/router/index.js
@@ -9,19 +9,9 @@ const router = createRouter({
       component: () => import("/src/views/Home.vue"),
     },
     {
-      path: "/test",
-      name: "test",
-      component: () => import("/src/views/Hometest.vue"),
-    },
-    {
       path: "/questionnaire",
       name: "questionnaire",
       component: () => import("/src/views/Questionnaire.vue"),
-    },
-    {
-      path: "/qtest",
-      name: "qtest",
-      component: () => import("/src/views/QTest.vue"),
     },
   ],
 });
