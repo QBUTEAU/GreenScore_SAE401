2d3fe69b93e95447530cc4c456ad0a837bcf3b70
Jathushan Selvarajah <126668649+Jathushanselvarajah@users.noreply.github.com>
2024-03-27 15:18:30 +0100

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,30 +1,20 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
-    <footer class="footer">
-        <p class="footer__copy">
-            &copy; 2024 GreenScore - Tous droits réservés
-        </p>
-        <ul class="footer__liens">
-            <li>
-                <RouterLink to="/">
-                    Accueil
-                </RouterLink>
-            </li>
-            <li>
-                <RouterLink to="/questionnaire">
-                    Questionnaire
-                </RouterLink>
-            </li>
-            <li>
-                <RouterLink to="/mentions">
-                    Mentions légales
-                </RouterLink>
-            </li>
-        </ul>
-    </footer>
+  <footer class="footer">
+    <p class="footer__copy">&copy; 2024 GreenScore - Tous droits réservés</p>
+    <ul class="footer__liens">
+      <li>
+        <RouterLink to="/"> Accueil </RouterLink>
+      </li>
+      <li>
+        <RouterLink to="/questionnaire"> Questionnaire </RouterLink>
+      </li>
+      <li>
+        <RouterLink to="/notice"> Mentions légales </RouterLink>
+      </li>
+    </ul>
+  </footer>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
