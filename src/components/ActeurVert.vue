c0936b1c2099bc7d91f3e97e96cd544fa5fb0fab
Quentin Buteau <buteauquentin10@gmail.com>
2024-04-10 10:38:13 +0200

    Mise en page Acteur Vert

--- src/components/ActeurVert.vue
+++ src/components/ActeurVert.vue
@@ -8,41 +8,36 @@ import Footer from "@/components/Footer.vue";
     <main class="results">
         <div class="results__level">
             <p>Suite à vos réponses, vous obtenez le titre de :</p>
-            <span class="results-level__span green">Défenseur Vert</span>
+            <span class="results-level__span green">Acteur Vert</span>
         </div>
         <div class="results__advices">
             <div class="results-advices__texte">
-                <p>Bravo pour votre engagement en faveur de l'écologie ! Votre détermination à protéger notre planète
-                    est
-                    admirable et vous met sur la voie pour devenir un véritable héros de l'environnement.
-                    Vos actions, telles que le tri des déchets et l'utilisation de compost, démontrent un engagement
-                    louable
-                    envers la durabilité.
+                <p>Félicitations pour votre engagement envers l'écologie ! Votre volonté de prendre des mesures pour
+                    protéger notre planète est un premier pas important vers un mode de vie plus durable. Bien que vous
+                    n'ayez pas encore atteint le statut d'Éco Leader, vous avez déjà pris des mesures significatives
+                    pour réduire votre empreinte environnementale.
                 </p>
-                <p>Pour aller encore plus loin dans votre démarche écologique, voici quelques conseils supplémentaires :
+                <p>Voici quelques conseils supplémentaires pour vous aider à progresser vers le niveau d'Éco Leader et à
+                    maximiser votre impact écologique :
                 </p>
                 <ul>
-                    <li>Continuez à utiliser des ampoules à économie d'énergie ou des LED pour réduire votre
-                        consommation
-                        d'électricité. Ces petites actions peuvent avoir un impact significatif sur votre empreinte
-                        carbone.
+                    <li>Poursuivez vos efforts pour réduire votre consommation d'énergie en utilisant des ampoules à
+                        économie d'énergie et en éteignant les appareils électroniques inutilisés. En adoptant de
+                        simples habitudes quotidiennes, vous pouvez contribuer à réduire votre empreinte carbone.
                     </li>
-                    <li>Cherchez des moyens d'intégrer des pratiques écologiques dans votre travail et votre vie
-                        quotidienne. Par exemple, vous pourriez encourager votre employeur à mettre en place des
-                        initiatives
-                        de durabilité sur le lieu de travail, comme le recyclage ou l'utilisation d'énergie
-                        renouvelable.
+                    <li>Engagez-vous à apprendre davantage sur les enjeux environnementaux et les solutions durables. En
+                        vous éduquant et en partageant vos connaissances avec d'autres, vous pouvez amplifier l'impact
+                        de vos actions.
                     </li>
                     <li>
-                        Continuez à vous informer sur les problèmes environnementaux et à sensibiliser votre entourage à
-                        ces
-                        questions. En partageant vos connaissances et en encourageant les autres à agir, vous pouvez
-                        multiplier l'impact de vos actions écologiques.
+                        Explorez des moyens de réduire votre dépendance aux combustibles fossiles en optant pour des
+                        modes de transport durables. En privilégiant les transports en commun, le vélo ou la marche,
+                        vous pouvez réduire les émissions de gaz à effet de serre associées à vos déplacements.
                     </li>
                 </ul>
-                <p>En suivant ces conseils, vous pouvez continuer à faire progresser votre engagement pour l'écologie et
-                    inspirer les autres autour de vous. Votre détermination à protéger notre planète est une source
-                    d'inspiration, et nous vous encourageons à poursuivre vos efforts pour un avenir plus durable.
+                <p>Continuez à faire de petits changements positifs dans votre vie quotidienne et à inspirer ceux qui
+                    vous entourent à faire de même. En travaillant ensemble, nous pouvons tous avoir un impact
+                    significatif sur la santé de notre planète.
                 </p>
                 <img class="feuille1" src="@/assets/img/feuille1.svg" alt="planet" />
                 <img class="feuille2" src="@/assets/img/feuille2.svg" alt="planet" />
